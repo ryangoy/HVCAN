d286fef498e280001eb1a3927654c21c3b67c6c9
Michael Zhao <mengzhe.zhao@gmail.com>
2017-10-07 15:39:55 -0700

    decoder noise

--- model.py
+++ model.py
@@ -78,12 +78,13 @@ class pix2pix(object):
         self.real_B = self.real_data[:, :, :, :self.input_c_dim]
         self.real_A = self.real_data[:, :, :, self.input_c_dim:self.input_c_dim + self.output_c_dim]
  
-        noise1 = tf.random_uniform(self.real_A.shape[:-1].as_list() + [1,])
-        noise2 = tf.random_uniform(self.real_A.shape[:-1].as_list() + [1,])
-        self.real_A_noisy1 = tf.concat([self.real_A, noise1], 3)
-        self.real_A_noisy2 = tf.concat([self.real_A, noise2], 3)
+        # noise1 = tf.random_uniform(self.real_A.shape[:-1].as_list() + [1,])
+        # noise2 = tf.random_uniform(self.real_A.shape[:-1].as_list() + [1,])
+        # self.real_A_noisy1 = tf.concat([self.real_A, noise1], 3)
+        # self.real_A_noisy2 = tf.concat([self.real_A, noise2], 3)
 
-        self.real_A2 = tf.concat([self.real_A_noisy1, self.real_A_noisy2], 0)
+        # self.real_A2 = tf.concat([self.real_A_noisy1, self.real_A_noisy2], 0)
+        self.real_A2 = tf.concat([self.real_A, self.real_A], 0)
 
         print '1 ' + str(self.real_A_noisy1.shape)
         print '2 ' + str(self.real_A_noisy2.shape)
@@ -284,7 +285,11 @@ class pix2pix(object):
             e8 = self.g_bn_e8(conv2d(lrelu(e7), self.gf_dim*8, name='g_e8_conv'))
             # e8 is (1 x 1 x self.gf_dim*8)
 
-            self.d1, self.d1_w, self.d1_b = deconv2d(tf.nn.relu(e8),
+            noise = tf.random_uniform(e8[:-1].as_list() + [1,])
+            e8_noisy = tf.concat([self.e8, noise], 3)
+            print e8_noisy.shape
+
+            self.d1, self.d1_w, self.d1_b = deconv2d(tf.nn.relu(e8_noisy),
                 [self.batch_size*2, s128, s128, self.gf_dim*8], name='g_d1', with_w=True)
             d1 = tf.nn.dropout(self.g_bn_d1(self.d1), 0.5)
             d1 = tf.concat([d1, e7], 3)
